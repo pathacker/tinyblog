1e79198184a6fb5dbd733ed3735750265be156ad
Pat Hacker <pat@picoworks.com>
2018-02-25 16:27:48 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+/*
+   Tinyblog is a super simple web blog.
+*/
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var (
+	tmpl template.Template
+	s    = `
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<h1>
+{{.S}}
+</h1>
+</body>
+</html>
+`
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		S string
+	}{
+		S: "found me!",
+	}
+	t := template.Must(template.New("foo").Parse(s))
+	err := t.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
